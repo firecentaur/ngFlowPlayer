dfcd3c9de6eb7e38a0fcbaa0b1d0e3af08011d45
Paul Preibisch <fire@b3dmultitech.com>
2014-09-04 12:52:38 -0700

    initial commit, app constructed with yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name flowplayerdirectiveApp
+ * @description
+ * # flowplayerdirectiveApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('flowplayerdirectiveApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flowplayerdirectiveApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the flowplayerdirectiveApp
+ */
+angular.module('flowplayerdirectiveApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
